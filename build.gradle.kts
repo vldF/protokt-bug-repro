07ab077cab9a8e91309c1dd34f02c714f83443e3
hheijkenskjold <hannes.heijkenskjold@dice.se>
2024-10-10 13:34:00 +0200

    Hello world project

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("com.toasttab.protokt") version "1.0.0-beta.2"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvmToolchain(21)
+}
+
+protokt {
+    generate {
+        all()
+    }
+}
+
+tasks.withType<JavaCompile> {
+    enabled = false
+}
+
+dependencies {
+    implementation("com.google.protobuf:protobuf-java:3.21.7")
+    implementation("com.toasttab.protokt:protokt-runtime-grpc-jvm:1.0.0-beta.2")
+    implementation("io.grpc:grpc-protobuf:1.64.0")
+    implementation("io.grpc:grpc-kotlin-stub:1.4.1")
+}
